0ace25eac6c9b38607dcfa7f6b6f912244b9a641
Beata <beata@jasinski.com>
2023-11-08 22:21:35 -0800

    login authentication

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const db = require('./data/database');
 const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
 const authRouter = require('./routes/auth.routes');
+const productsRoutes = require('./routers/products.routes');
+const baseRoutes = require('./routes/base.routes');
 
 const app = express();
 
@@ -25,7 +27,9 @@ app.use(csrf());
 
 app.use(addCsrfTokenMiddleware);
 
+app.use(baseRoutes);
 app.use(authRouter);
+app.use(productsRoutes);
 
 app.use(errorHandlerMiddleware);
 
