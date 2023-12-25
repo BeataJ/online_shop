e1beae34e825b6fc300b2bfcec26c0677a72ec75
Beata <beata@jasinski.com>
2023-12-24 23:23:02 -0800

    update product as admin

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,53 +1,68 @@
 const Product = require('../models/product.model');
 
 async function getProducts(req, res, next) {
-    try { 
-        const products = await Product.findAll();
-        res.render('admin/products/all-products', {products: products});
-
-    } catch(error) {
-        next(error);
-        return;
-    }
+  try {
+    const products = await Product.findAll();
+    res.render('admin/products/all-products', { products: products });
+  } catch (error) {
+    next(error);
+    return;
+  }
 }
 
 function getNewProduct(req, res) {
-    res.render('admin/products/new-products');
+  res.render('admin/products/new-products');
 }
 
 async function createNewProduct(req, res, next) {
-    const product = new Product({
-        ...req.body,
-        image: req.file.filename
-    });
-    
-    try{
-        await product.save()
-    } catch(error) {
-        next(error);
-        return;
-    }
+  const product = new Product({
+    ...req.body,
+    image: req.file.filename,
+  });
 
-    res.redirect('/admin/products');
+  try {
+    await product.save();
+  } catch (error) {
+    next(error);
+    return;
+  }
+
+  res.redirect('/admin/products');
 }
 
 async function getUpdateProduct(req, res, next) {
-    try {
-        const product = await Product.findById(req.params.id);
-        res.render('admin/products/update-product', {product: product})
-    } catch(error) {
-        next(error);
-    }
-};
+  try {
+    const product = await Product.findById(req.params.id);
+    res.render('admin/products/update-product', { product: product });
+  } catch (error) {
+    next(error);
+  }
+}
 
-function updateProduct() {}
+async function updateProduct(req, res, next) {
+  const product = new Product({
+    ...req.body,
+    _id: req.params.id,
+  });
 
+  if (req.file) {
+    product.replaceImage(req.file.filename);
+  }
 
+  try {
+    await product.save();
+  } catch (error) {
+    next(error);
+    return;
+  }
+  
+  res.redirect('/admin/products');
+}
 
 module.exports = {
-    getProducts: getProducts,
-    getNewProduct: getNewProduct,
-    createNewProduct: createNewProduct,
-    getUpdateProduct: getUpdateProduct,
-    updateProduct: updateProduct
-}
+  getProducts: getProducts,
+  getNewProduct: getNewProduct,
+  createNewProduct: createNewProduct,
+  getUpdateProduct: getUpdateProduct,
+  updateProduct: updateProduct,
+};
